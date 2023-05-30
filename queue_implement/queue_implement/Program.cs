0e5d59a9cde96315d05912e53cd1979e1209380a
Mostafa Elkhamy <81165303+ELKHAMY@users.noreply.github.com>
2023-05-30 23:58:36 +0300

    Add files via upload

---
+++ queue_implement/queue_implement/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Runtime.InteropServices;
+
+namespace queue_implement;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+     Queue queue = new Queue(5);
+        queue.enqueue(2);
+        queue.enqueue(3);
+        queue.enqueue(4);
+        queue.enqueue(9);
+        queue.enqueue(8);
+
+        queue.dequeue();
+        queue.dequeue();
+
+        queue.printQueue();
+      
+
+
+
+    }
+}
+
---
+++ queue_implement/queue_implement/Queue.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace queue_implement
+{
+    class Queue
+    {
+        #region fieldsss
+        private int[] arr;
+        private int front;
+        private int rear;
+        private int max;
+        #endregion
+
+        #region constructor
+        public Queue(int size)
+        {
+            arr = new int[size];
+            front = 0;
+            rear = -1;
+            max = size;
+        }
+        #endregion
+
+        #region enqueue
+        public void enqueue(int item)
+        {
+            if (rear == max - 1)
+            {
+                Console.WriteLine("completed");
+                return;
+            }
+            else
+            {
+                arr[++rear] = item;
+            }
+        }
+        #endregion
+
+        #region dequeue
+
+        public int dequeue()
+        {
+            if (front == rear + 1)
+            {
+                Console.WriteLine("empty");
+                return -1;
+            }
+            else
+            {
+                return arr[front++];
+            }
+        }
+        #endregion
+
+        #region printqueue
+        public void printQueue()
+        {
+            if (front == rear + 1)
+            {
+                Console.WriteLine("Empty");
+                return;
+            }
+            else
+            {
+                for (int i = front; i <= rear; i++)
+                {
+                    Console.WriteLine( arr[i]);
+                }
+            }
+
+
+        }
+        #endregion
+    }
+}
